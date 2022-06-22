e646d4555ac8317395659fc1c663e0e2652f4022
ryandewillis <dewillisr@yahoo.com>
2022-06-22 15:59:08 -0500

    Update nav bar

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,14 +1,15 @@
 import { IoSearch } from "react-icons/io5";
+import { IoMdArrowDropdown } from 'react-icons/io';
 
 export default function Navbar() {
   return (
     <header className="w-screen px-10 py-4 flex justify-between items-center bg-neutral-800 text-neutral-50">
       <p className="text-3xl">DL Sports</p>
-      <div className="text-lg flex items-center space-x-3">
-        <IoSearch size="1.25em"/>
-        <p>Podcasts</p>
-        <p>Contact</p>
-      </div>
+      <IoSearch size="1.25em"/>
+      <p className="flex items-center">Articles <IoMdArrowDropdown/></p>
+      <p>Columns</p>
+      <p>&quot;On the DL&quot;</p>
+      <p>Contact</p>
     </header>
   )
 }
