89bd6e77d3278f0f9c7fc4015afc41c631ae9b09
ryandewillis <dewillisr@yahoo.com>
2022-06-26 18:53:14 -0500

    Improve display and functionality of podcast page

--- pages/podcast.tsx
+++ pages/podcast.tsx
@@ -1,37 +1,48 @@
 import Head from 'next/head';
 import Script from 'next/script';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { spotifyAPI } from '../additional';
 import restClient from '../utils/apollo-client-rest';
 import { gql } from '@apollo/client';
 import { Button } from '@material-tailwind/react';
 
+type Episode = {
+  id: string
+  name: string
+  html_description: string
+}
 type PodcastProps = {
-  episodes: {
-    id: string
-    name: string
-  }[]
+  episodes: Episode[]
+}
+const spotifyPlayerOptions = {
+  height: '160px',
+  width: '450px',
 }
 
-const switchEpisodes = (embedController: { loadUri: (uri: string) => void }) => {
-  document.querySelectorAll('#other-episodes > button').forEach(
-  episode => {
-    episode.addEventListener('click', () => {
-      embedController.loadUri(`spotify:episode:${episode.id}`)
-    });
-  });
-};
+const EpisodeText = ({ episode }: { episode: Episode }) => {
+  if (!episode) {
+    return null;
+  }
+  return (
+    <div
+      dangerouslySetInnerHTML={{ __html: `<b>${episode.name}</b>`.concat(episode.html_description) }}
+      className="mt-4 p-1 md:h-[324px] md:overflow-y-scroll"
+    />
+  );
+}
 
 export default function Podcast({ episodes }: PodcastProps) {
+  const [selectedEpisode, setSelectedEpisode] = useState(episodes?.[0]);
+  const [embedController, setEmbedController] = useState<{ loadUri: (uri: string) => void } | null>(null);
+
   useEffect(() => {
     window.onSpotifyIframeApiReady = (iFrameAPI: spotifyAPI) => {
       const element = document.getElementById('embed-iframe');
       const options = {
-        height: '160px',
-        width: '450px',
-        uri: `spotify:episode:${episodes[0].id}`
+        ...spotifyPlayerOptions,
+        uri: `spotify:episode:${episodes?.[0].id}`
       };
-      iFrameAPI.createController(element, options, switchEpisodes);
+      iFrameAPI.createController(element, options, setEmbedController);
     };
   })
 
@@ -45,16 +56,29 @@ export default function Podcast({ episodes }: PodcastProps) {
 
       <Script src="https://open.spotify.com/embed-podcast/iframe-api/v1" async/>
 
-      <div className="my-20 md:px-48 w-screen">
-        <h1 className="mt-4 mb-10 md:mb-6 text-3xl text-center">&quot;On the DL&quot; Podcast</h1>
-        <div className="flex flex-col md:flex-row gap-16 md:gap-24 justify-between">
-          <div id="embed-iframe" className="max-w-screen"></div>
+      <div className="my-20 w-screen">
+        <h1 className="mt-4 mb-10 text-3xl text-center">&quot;On the DL&quot; Podcast</h1>
+        <div className="flex flex-col md:flex-row gap-16 md:gap-24 justify-center">
+          <div className="w-full lg:w-[450px] max-w-screen">
+            <div id="embed-iframe"></div>
+            <EpisodeText episode={selectedEpisode} />
+          </div>
 
           <div className="w-full lg:w-[450px] max-w-screen h-[500px] bg-grey-200">
-            <div className="flex flex-col gap-6" id="other-episodes">
+            <div className="p-4 flex flex-col gap-6" id="other-episodes">
               <h3 className="text-center text-xl">Other Episodes</h3>
               {episodes.map((episode) => (
-                <Button key={episode.id} className="w-full h-16" id={episode.id}>
+                <Button
+                  key={episode.id}
+                  className="w-full h-16 px-2"
+                  id={episode.id}
+                  onClick={() => {
+                    if (embedController) {
+                      setSelectedEpisode(episode);
+                      embedController.loadUri(`spotify:episode:${episode.id}`);
+                    }
+                  }}
+                >
                   {episode.name}
                 </Button>
               ))}
@@ -67,7 +91,7 @@ export default function Podcast({ episodes }: PodcastProps) {
 }
 
 export async function getStaticProps() {
-  const showId = "6f2oD3RtQY1rOeyfF2OeOa";
+  const showId = process.env.SPOTIFY_SHOW_ID;
   const { data } = await restClient.query({
     query: gql`
         query podcastEpisodes {
@@ -75,7 +99,7 @@ export async function getStaticProps() {
                 items {
                     id
                     name
-                    description
+                    html_description
                 }
             }
         }
