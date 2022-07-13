c5b73a76a4ec4e7a0b490f6574962d4af751234f
ryandewillis <dewillisr@yahoo.com>
2022-07-13 00:36:08 -0500

    Add head tags to each page

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,14 +2,23 @@ import 'styles/globals.css'
 import Layout from "components/layout";
 import { AppProps } from 'next/app';
 import { ThemeProvider } from '@material-tailwind/react';
+import Head from 'next/head';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ThemeProvider>
-      <Layout navbarProps={{...pageProps.navbar}}>
-        <Component {...pageProps} />
-      </Layout>
-    </ThemeProvider>
+    <>
+      <Head>
+        <title>DL Sports</title>
+        <link rel="icon" href="/favicon.ico" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+        <meta charSet="UTF-8"/>
+      </Head>
+      <ThemeProvider>
+        <Layout navbarProps={{...pageProps.navbar}}>
+          <Component {...pageProps} />
+        </Layout>
+      </ThemeProvider>
+    </>
   )
 }
 
