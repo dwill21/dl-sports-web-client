c8d485919736dcdc030d8beb0f286a5cd0f92397
ryandewillis <dewillisr@yahoo.com>
2022-07-27 19:56:55 -0500

    Create custom hook for checking if screen is small

--- components/navbar/navbar.tsx
+++ components/navbar/navbar.tsx
@@ -1,12 +1,11 @@
 import { IconButton, Navbar } from '@material-tailwind/react';
-import { useEffect, useMemo, useState } from 'react';
 import TypographyLink from 'components/typography-link';
 import NavbarContent from 'components/navbar/navbar-content';
 import { NavbarProps } from 'additional';
-import debounce from 'lodash.debounce';
 import { IoSearchSharp } from 'react-icons/io5';
 import { useRouter } from 'next/router';
 import HamburgerMenu from 'components/navbar/hamburger-menu';
+import { useSmallScreen } from 'utils/hooks/use-small-screen';
 
 const navLinks = [
   {
@@ -25,18 +24,7 @@ const navLinks = [
 
 export default function AppNavbar({ sports }: NavbarProps) {
   const router = useRouter();
-  const [smallScreen, setSmallScreen] = useState<boolean | null>(null);
-  const handleWindowResize = () => setSmallScreen(window.innerWidth < 768);
-  const debouncedResizeHandler = useMemo(() => debounce(handleWindowResize, 300), []);
-
-  useEffect(() => {
-    handleWindowResize();
-    window.addEventListener('resize', debouncedResizeHandler as EventListener);
-    return () => {
-      debouncedResizeHandler.cancel();
-      window.removeEventListener('resize', debouncedResizeHandler as EventListener);
-    }
-  }, [debouncedResizeHandler]);
+  const isSmallScreen = useSmallScreen();
 
   return (
     <Navbar fullWidth={true}>
@@ -45,7 +33,7 @@ export default function AppNavbar({ sports }: NavbarProps) {
           DL Sports
         </TypographyLink>
 
-        {smallScreen !== null && (smallScreen ?
+        {isSmallScreen !== null && (isSmallScreen ?
           <div className="flex items-center">
             <HamburgerMenu sports={sports} navItems={navLinks}/>
             <IconButton variant="text" onClick={() => router.push('/search')}>
