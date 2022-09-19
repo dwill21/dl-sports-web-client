d6e893ed7b95fb60e64883d48b07e1a2e1c7760e
ryandewillis <dewillisr@yahoo.com>
2022-09-19 11:40:14 -0500

    Rename APP_HOST to IMAGES_DOMAIN

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: [process.env.APP_HOST, 'img.youtube.com'],
+    domains: [process.env.IMAGES_DOMAIN, 'img.youtube.com'],
   },
 }
 
